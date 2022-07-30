37dfe0e856c9b0c1e1470a85c66d7fca1932543f
pugaivan <unsaightly@gmail.com>
2022-07-31 00:07:07 +0300

    refactor code

--- models/contactsRelations.js
+++ models/contactsRelations.js
@@ -1,23 +1,14 @@
 const connection = require('../mysql')
 
 exports.getContactRelations = function (userId, contactId, res) {
-  getUserId(userId, contactId, (id) => {
-    if (!id) {
+  const query = `SELECT id   FROM contacts_relations  WHERE user_id = '${userId.id}' AND contact_id = '${contactId}';`
+  connection.query(query, (err, rows, fields) => {
+    if (!rows.length) {
       insertContactRelations(userId, contactId)
       res.sendStatus(200)
     } else {
       res.status(400).json({ errorMessage: "contact with this number already in your contact list" })
     }
-  })
-
-}
-
-
-function getUserId(userId, contactId, callback) {
-  const query = `SELECT id   FROM contacts_relations  WHERE user_id = '${userId.id}' AND contact_id = '${contactId}';`
-  connection.query(query, (err, rows, fields) => {
-    const id = rows.length ? rows[0].id : null
-    callback(id)
   })
 }
 
